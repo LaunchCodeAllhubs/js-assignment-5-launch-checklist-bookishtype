54b53f64ca2b6cedbd9e23f2a63d5e10618b02de
Faith <fmschroeder98@gmail.com>
2023-08-13 05:13:41 -0500

    finish script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Write your JavaScript code here!
+
 window.addEventListener("load", function() {  
     let list = document.getElementById("faultyItems");
     list.style.visibility = "hidden";
@@ -10,5 +11,20 @@ listedPlanetsResponse.then(function (result) {
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
-
+    let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    let pilot = pilotNameInput.value;
+    let copilotNameInput = document.querySelector("input[name=copilotName]");
+    let copilot = copilotNameInput.value;
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = Number(fuelLevelInput.value);
+    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+    let cargoLevel = Number(cargoMassInput.value);
+    
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    event.preventDefault();
+   });
+   
+});
    
