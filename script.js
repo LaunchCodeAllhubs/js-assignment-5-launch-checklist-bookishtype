24a792439d7ab78b54f94f426da2428d49a921f0
Faith <fmschroeder98@gmail.com>
2023-08-13 05:12:23 -0500

    still getting error

--- script.js
+++ script.js
@@ -10,17 +10,5 @@ listedPlanetsResponse.then(function (result) {
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
-window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
    
-});
