aade4c96b7c7577b2ce5a66d439fd3c5e3bb6c17
Faith <112731014+bookishtype@users.noreply.github.com>
2023-08-14 09:21:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ window.addEventListener("load", function() {
     let list = document.getElementById("faultyItems");
     list.style.visibility = "hidden";
     let listedPlanets;
-let listedPlanetsResponse = myFetch()
+    let listedPlanetsResponse = myFetch()
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
 listedPlanetsResponse.then(function (result) {
     listedPlanets = result; 
 }).then(function () {
+    // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     let planet = pickPlanet(listedPlanets); 
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
 }) 
