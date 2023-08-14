9c8c0eb0593583f0745d72dea732698cdf4fa69d
Faith <fmschroeder98@gmail.com>
2023-08-14 12:37:03 -0500

    image error

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,24 +1,18 @@
-// Write your helper functions here!
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   
-   let missionTarget = document.getElementById("missionTarget")
-   missionTarget.innerHTML = `
-                            <h2>Mission Destination</h2>
-                            <ol>
-                                <li>Name: ${name}</li>
-                                <li>Diameter: ${diameter}</li>
-                                <li>Star: ${star}</li>
-                                <li>Distance from Earth: ${distance}</li>
-                                <li>Number of Moons: ${moons}</li>
-                            </ol>
-                            <img src="${imageUrl}">
-                            `;
-                
+    let missionTarget = document.getElementById("missionTarget");
+    missionTarget.innerHTML = `
+                        <h2>Mission Destination</h2>
+                        <ol>
+                            <li>Name: ${name}</li>
+                            <li>Diameter: ${diameter}</li>
+                            <li>Star: ${star}</li>
+                            <li>Distance from Earth: ${distance}</li>
+                            <li>Number of Moons: ${moons}</li>
+                        </ol>
+                        <img src="${imageUrl}">`
 }
-
 
 function validateInput(testInput) {
     if(testInput === "") {
